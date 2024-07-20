58d33e61aa465bf626ca0b6c4f27c0904b942b0f
Ben North <ben.north@tcd.ie>
2024-07-20 18:16:38 +0100

    create_clone_of(): Use _effective_source_object()

--- src/lib/pytch/clone.py
+++ src/lib/pytch/clone.py
@@ -1,5 +1,5 @@
 import copy
-from pytch.syscalls import register_sprite_instance
+from pytch.syscalls import _effective_source_object, register_sprite_instance
 
 
 def create_clone_of(original_cls_or_obj):
@@ -9,19 +9,7 @@ def create_clone_of(original_cls_or_obj):
     instance.  If a class, we clone its instance-0.  If an instance,
     clone that instance.
     """
-    if isinstance(original_cls_or_obj, type):
-        if not hasattr(original_cls_or_obj, "_pytch_parent_project"):
-            raise ValueError("can only clone a Pytch-registered class")
-
-        # Would be surprising if this fails, but handle anyway.
-        try:
-            obj = original_cls_or_obj.the_original()
-        except:
-            raise RuntimeError("the_original() failed")
-
-    else:
-        obj = original_cls_or_obj
-
+    obj = _effective_source_object(original_cls_or_obj)
     return create_clone_of_instance(obj)
 
 
